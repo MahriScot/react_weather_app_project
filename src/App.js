3ecd7362956d06a47c1689884edb9fe161973808
MahriScot <mahristewart@hotmail.co.uk>
2022-08-29 18:26:54 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://steady-youtiao-11dde2.netlify.app" target="_blank">
-          Mahri Stewart
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MahriScot/react_weather_app_project"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://velvety-praline-d736cc.netlify.app" target="_blank">
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://steady-youtiao-11dde2.netlify.app" target="_blank">
+            Mahri Stewart
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MahriScot/react_weather_app_project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://velvety-praline-d736cc.netlify.app" target="_blank">
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
