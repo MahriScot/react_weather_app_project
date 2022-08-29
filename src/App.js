38ba099c07db2af539491aaf295b584f8a44eeee
MahriScot <mahristewart@hotmail.co.uk>
2022-08-29 18:05:58 +0100

    added footer - github, netlify, and my portfolio links

--- src/App.js
+++ src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://steady-youtiao-11dde2.netlify.app" target="_blank">
+          Mahri Stewart
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MahriScot/react_weather_app_project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://velvety-praline-d736cc.netlify.app" target="_blank">
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
