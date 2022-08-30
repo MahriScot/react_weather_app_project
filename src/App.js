0792bbe0938412ef6447f97e2095b6544445de30
MahriScot <mahristewart@hotmail.co.uk>
2022-08-30 12:41:04 +0100

    adding rel="noreferrer" to target="_blank"

--- src/App.js
+++ src/App.js
@@ -10,18 +10,27 @@ export default function App() {
         <Weather defaultCity="Berlin" />
         <footer>
           This project was coded by{" "}
-          <a href="https://steady-youtiao-11dde2.netlify.app" target="_blank">
+          <a
+            href="https://steady-youtiao-11dde2.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
             Mahri Stewart
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/MahriScot/react_weather_app_project"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>{" "}
           and hosted on{" "}
-          <a href="https://velvety-praline-d736cc.netlify.app" target="_blank">
+          <a
+            href="https://velvety-praline-d736cc.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
             Netlify
           </a>
         </footer>
