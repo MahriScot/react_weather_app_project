f736fe5d3931c42117fc83870a1d97700fd1b84e
MahriScot <mahristewart@hotmail.co.uk>
2022-08-31 12:32:20 +0100

    image upload to header and footer

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <p>There's no such thing as bad weather, only bad clothing</p>
+        <p>
+          <img src="images/raining.svg" alt="raining" width="60" /> There's no
+          such thing as bad weather, only bad clothing{" "}
+          <img src="images/walking.svg" alt="walking" width="60" />
+        </p>
+
         <Weather defaultCity="Edinburgh" />
         <footer>
-          This project was coded by{" "}
+          <img src="images/terrain.png" alt="my_logo" width="20" /> This project
+          was coded by{" "}
           <a
             href="https://steady-youtiao-11dde2.netlify.app"
             target="_blank"
