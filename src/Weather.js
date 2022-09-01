77ea2641b7b759f729ac2f5f08021035c142e949
MahriScot <mahristewart@hotmail.co.uk>
2022-09-01 16:14:53 +0100

    setting up component for the future forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import WeatherFutureForecast from "./WeatherFutureForecast";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       date: new Date(response.data.dt * 1000),
       city: response.data.name,
       description: response.data.weather[0].description,
@@ -20,20 +22,21 @@ export default function Weather(props) {
     });
   }
 
-  function search() {
-    const apiKey = "1cea906f8f3ab268b1c4225a33a9637a";
-    const units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
   function handleSubmit(event) {
     event.preventDefault();
-    search(city);
+    search();
   }
 
   function handleCityChange(event) {
     setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "1cea906f8f3ab268b1c4225a33a9637a";
+    const units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
@@ -44,7 +47,7 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city..."
+                placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="on"
                 onChange={handleCityChange}
@@ -60,7 +63,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherFutureForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
@@ -68,3 +71,6 @@ export default function Weather(props) {
     return "Loading...";
   }
 }
+
+// const apiKey = "1cea906f8f3ab268b1c4225a33a9637a";
+//
